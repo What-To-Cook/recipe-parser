55b481f53fccc7399ec6012edbff53d0f1bb4303
Timur Kasimov <timurcat7@gmail.com>
2023-01-05 05:08:47 +0700

    Init commit

---
+++ recipe_parser/parsing/parse.py
@@ -0,0 +1,73 @@
+import json
+from pathlib import Path
+from typing import Optional
+
+import requests
+from loguru import logger
+
+from recipe_parser.custom_types import JSON
+from recipe_parser.parsing.page import get_urls_from_page
+from recipe_parser.parsing.recipe import parse_recipe
+
+
+def _process_url_with_retries(
+    url: str,
+    max_retries: int,
+) -> Optional[JSON]:
+    for _ in range(max_retries):
+        try:
+            recipe = parse_recipe(url)
+        except requests.RequestException:
+            logger.error(f'unable to process url {url}')
+
+            return None
+
+        return recipe
+
+
+def _process_urls(
+    urls: list[str],
+    max_retries: int,
+) -> list[JSON]:
+    recipes = []
+
+    for url in urls:
+        recipe = _process_url_with_retries(
+            url,
+            max_retries,
+        )
+
+        if recipe is not None:
+            recipes.append(recipe)
+
+    return recipes
+
+
+def parse(
+    num_pages: int,
+    max_retries: int,
+    out_path: Path,
+) -> None:
+    recipes = []
+
+    for page in range(1, num_pages + 1):
+        recipes.extend(
+            _process_urls(
+                get_urls_from_page(page),
+                max_retries,
+            ),
+        )
+
+    out_path.mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+
+    for idx, recipe in enumerate(recipes):
+        with open(out_path / f'{idx}.json', 'w', encoding='utf-8') as out:
+            json.dump(
+                recipe,
+                out,
+                indent=4,
+                ensure_ascii=False,
+            )
