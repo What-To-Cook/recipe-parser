710b5ea082ec230fcc53f06a0ff52ca1c2cb5923
Timur Kasimov <timurcat7@gmail.com>
2023-01-06 02:18:48 +0700

    Added functionality for unique recipes. Rewrite connection and recipes insertion

--- recipe_parser/db.py
+++ recipe_parser/db.py
@@ -1,45 +1,67 @@
-from typing import Union, Optional
+import os
+from typing import Union
+from urllib.parse import quote_plus
 
 from pymongo import MongoClient
 
 from recipe_parser.custom_types import JSON
 
+_INGREDIENTS_KEY = 'ingredients'
+
 
 class MongoDatabase:
-    def __init__(
+    def __init__(  # noqa: WPS211
         self,
         host: str,
         port: int,
         *,
-        default_db_name: str,
-        default_collection: str,
+        db: str,
+        recipes_collection: str,
+        ingredients_collection: str,
     ) -> None:
-        self._default_db_name = default_db_name
-        self._default_collection = default_collection
+        self._db = db
+        self._recipes_collection = recipes_collection
+        self._ingredients_collection = ingredients_collection
 
         self._client = MongoClient(
-            host=host,
-            port=port,
+            self._get_connection_string(
+                host,
+                port,
+            ),
         )
 
     def insert_recipes(
         self,
         recipe: Union[JSON, list[JSON]],
-        *,
-        db_name: Optional[str] = None,
-        collection: Optional[str] = None,
     ) -> None:
-        if db_name is None and collection is not None:
-            raise ValueError('collection is None when db_name is specified')
-
-        if db_name is not None and collection is None:
-            raise ValueError('db_name is None when collection is specified')
-
-        if db_name is None and collection is None:
-            db_name = self._default_db_name
-            collection = self._default_collection
-
         if not isinstance(recipe, list):
             recipe = [recipe]
 
-        self._client[db_name][collection].insert_many(recipe)
+        self._client[self._db][self._recipes_collection].insert_many(recipe)
+        self._client[self._db][self._ingredients_collection].update_one(
+            {},
+            {'$set': {_INGREDIENTS_KEY: self._gather_ingredients(recipe)}},
+            upsert=True,
+        )
+
+    def _gather_ingredients(
+        self,
+        recipes: list[JSON],
+    ) -> list[str]:
+        ingredients = self._client[self._db][self._ingredients_collection].find_one()
+        ingredients = set() if ingredients is None else set(ingredients[_INGREDIENTS_KEY])
+
+        for recipe in recipes:
+            ingredients.update(recipe['unique_ingredients'])
+
+        return list(ingredients)
+
+    @staticmethod
+    def _get_connection_string(
+        host: str,
+        port: int,
+    ) -> str:
+        user = quote_plus(os.environ['MONGO_USER'])
+        password = quote_plus(os.environ['MONGO_PASSWORD'])
+
+        return f'mongodb://{user}:{password}@{host}:{port}/'  # noqa: WPS221
