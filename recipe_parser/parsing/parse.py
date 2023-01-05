1b6d013eabff7916852779b7328782ecb916e716
Timur Kasimov <timurcat7@gmail.com>
2023-01-06 02:19:47 +0700

    Added db usage

--- recipe_parser/parsing/parse.py
+++ recipe_parser/parsing/parse.py
@@ -6,11 +6,12 @@ import requests
 from loguru import logger
 
 from recipe_parser.custom_types import JSON
-from recipe_parser.parsing.page import get_urls_from_page
+from recipe_parser.db import MongoDatabase
+from recipe_parser.parsing.page import get_urls_from_page  # type: ignore
 from recipe_parser.parsing.recipe import parse_recipe
 
 
-def _process_url_with_retries(
+def _process_url_with_retries(  # type: ignore
     url: str,
     max_retries: int,
 ) -> Optional[JSON]:
@@ -47,6 +48,7 @@ def parse(
     num_pages: int,
     max_retries: int,
     out_path: Path,
+    db: Optional[MongoDatabase] = None,
 ) -> None:
     recipes = []
 
@@ -71,3 +73,6 @@ def parse(
                 indent=4,
                 ensure_ascii=False,
             )
+
+        if db is not None:
+            db.insert_recipes(recipe)
